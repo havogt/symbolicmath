c1aada357128991334f14a9f3f8460bcd5613469
Hannes Vogt <hannes@havogt.de>
2016-03-25 20:15:59 +0100

    fix redefinition error in clang and gcc < 5 compilers

--- include/compare.h
+++ include/compare.h
@@ -8,27 +8,19 @@
 #ifndef COMPARE_H_
 #define COMPARE_H_
 
-template <class... Ts> using void_t = void;
+template <typename... Ts> using void_t = void;
 
 template<typename T1, typename T2, typename = void> struct LessType{};
 template<typename T1, typename T2, typename = void> struct LessValue{};
 
 template<typename T1, typename T2, typename = void> struct Less
 {
-	using type = typename LessValue<T1,T2>::type;
+	using type = typename LessType<T1,T2>::type;
 };
 
-template<typename T1, typename T2> struct Less<T1,T2,void_t<typename T1::nested_type, Int<T2:: compare_value> > >
+template<typename T1, typename T2> struct Less<T1,T2,void_t<Int<T1:: compare_value>, Int<T2:: compare_value> > >
 {
-	using type = typename LessType<T1,T2>::type;
-};
-template<typename T1, typename T2> struct Less<T1,T2,void_t<Int<T1:: compare_value>, typename T2::nested_type > >
-{
-	using type = typename LessType<T1,T2>::type;
-};
-template<typename T1, typename T2> struct Less<T1,T2,void_t<typename T1::nested_type, typename T2::nested_type> >
-{
-	using type = typename LessType<T1,T2>::type;
+	using type = typename LessValue<T1,T2>::type;
 };
 
 template<typename T1, typename T2> struct LessType<T1,T2,typename std::enable_if<(T1::category <= T2::category) && !(T1::category >= T2::category)>::type>
@@ -42,14 +34,12 @@ template<typename T1, typename T2> struct LessType<T1,T2,typename std::enable_if
 	using type = typename Less<typename T1::nested_type, typename T2::nested_type>::type;
 };
 
-template<typename T1, typename T2> struct LessValue<T1,T2, typename std::enable_if<(T1::compare_value <= T2::compare_value) && !(T1::compare_value >= T2::compare_value)>::type >
-//	: std::true_type
-	  {
+template<typename T1, typename T2> struct LessValue<T1,T2, typename std::enable_if<(T1::compare_value >= T2::compare_value) && !(T1::compare_value <= T2::compare_value)>::type >
+{
 	using type = std::true_type;
-	  };
+};
 
-template<typename T1, typename T2> struct LessValue<T1,T2, typename std::enable_if<T1::compare_value >= T2::compare_value>::type >
-//	: std::false_type{};
+template<typename T1, typename T2> struct LessValue<T1,T2, typename std::enable_if<T1::compare_value <= T2::compare_value>::type >
 {
 	using type = std::false_type;
 };
